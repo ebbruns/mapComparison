243b475da2621d126f8373fc89ea44a7d8fb2f30
Evan Bruns <evan.bruns@gmail.com>
2016-11-13 14:48:33 -0800

    sorted out gitignore for pyCharm, still fighting with dateTimes to get comparisons started

---
+++ makeMapList.py
@@ -0,0 +1,23 @@
+# This is a tool to keep MapCompare current with changes made to the Citatsia Maplist.
+# Ideally this would be done each time a comparison is made, but there's no way of knowing that both people in a
+# given comparison have beaten all of the maps between them.
+
+
+
+#BUGGED: CURRENTLY STICKS COMMAS ON ENDS OF MAP NAMES
+def mapList():
+
+    full_times = input("Please enter the times of someone who has beaten all the maps to generate a maplist.")
+    full_list = full_times.split(" ")
+    maps_list = list()
+
+    print(full_list)
+
+    for entry in full_list:
+        if entry.startswith("surf_"):
+            maps_list.append(entry)
+
+    print(maps_list)
+
+mapList()
+
